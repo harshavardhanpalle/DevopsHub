83b33a9b09ad4817d52dd36a55945cc8bd2cafcd
harshavardhanpalle <harshavardhanpalle.data@gmail.com>
2026-08-21 09:01:16 +0530

    Fix db-init script: specify --dbname explicitly and make script executable

--- scripts/db-init/01-create-databases.sh
+++ scripts/db-init/01-create-databases.sh
@@ -6,7 +6,7 @@
 set -e
 
 for DB in userdb blogdb categorydb notificationdb; do
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
         SELECT 'CREATE DATABASE $DB' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB')\gexec
 EOSQL
 done
