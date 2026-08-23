f00aec9c787eae25e4dbf9fccd2ade61a84995f9
P HARSHAVARDHAN <harshavardhanpalle.data@gmail.com>
2026-08-23 15:59:36 +0530

    Update main.tf

--- infrastructure/terraform/modules/rds/main.tf
+++ infrastructure/terraform/modules/rds/main.tf
@@ -63,7 +63,7 @@ resource "aws_db_instance" "this" {
 
   db_subnet_group_name   = aws_db_subnet_group.this.name
   vpc_security_group_ids = [var.security_group_id]
-  publicly_accessible    = false
+  publicly_accessible    = true
 
   multi_az                  = var.multi_az
   backup_retention_period   = var.backup_retention_period
